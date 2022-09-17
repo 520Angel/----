2ff3b6d5f4df1d07db4f76bcc0ab066d4dd62bc9
520Angel <110648296+520Angel@users.noreply.github.com>
2022-09-17 12:12:46 +0800

    new

---
+++ Z组件封装/a/src/components/vantCrads.vue
@@ -0,0 +1,60 @@
+<template>
+	<div>
+		<div class="card">
+			<div class="card-img">
+				<img src="../assets/logo.png" alt="">
+			</div>
+			<div class="card-des">
+				<p>{{title}}</p>
+				<p>{{des}}</p>
+				<slot name="tag"></slot>
+				<p @click="chuan">{{pay}}</p>
+			</div>
+		</div>
+	</div>
+</template>
+
+<script>
+	export default{
+		data(){
+			return{
+				msg:'aaa'
+			}
+		},
+		props:{
+			title:{
+				type:String,
+				default:'这是电影'
+			},
+			des:{
+				type:String,
+				default:''
+			},
+			pay:{
+				type:String,
+				default:''
+			}
+		},
+		methods:{
+			chuan(){
+				this.$emit('comm',this.msg)
+			}
+		}
+	}
+</script>
+
+<style>
+	.card{
+		display: flex;
+	}
+	.card-img{
+		width: 40%;
+	}
+	.card-img img{
+		width: 100%;
+	}
+	.card-des{
+		flex: 1;
+		text-align: left;
+	}
+</style>
---
+++ Z组件封装/a/src/views/AboutView.vue
@@ -0,0 +1,29 @@
+<template>
+	<div class="about">
+		<h1>This is an about page</h1>
+		<vant-crads title="这是夏洛特" :des='aaa' pay='12' @comm='msg'>
+			<div slot="tag">
+				<span>{{mm}}</span>
+			</div>
+		</vant-crads>
+	</div>
+</template>
+<script>
+	import vantCrads from '../components/vantCrads.vue'
+	export default{
+		components:{
+			vantCrads
+		},
+		data(){
+			return{
+				aaa:'这是喜剧片',
+				mm:'没窗'
+			}
+		},
+		methods:{
+			msg(e){
+				console.log(e);
+			}
+		}
+	}
+</script>
